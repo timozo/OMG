f63b63cc5886fe3520b61808eeec885e795fbad7
Alexandre Zürcher <alexandre.zurcher@ik.me>
2023-12-09 00:39:14 +0200

    adding draft of Search and Table

---
+++ src/views/Search.jsx
@@ -0,0 +1,11 @@
+import { Diversity1Rounded } from "@mui/icons-material";
+import Table from "../components/Table";
+import React from "react";
+
+export default function Search() {
+    return (
+        <div>
+            <Table></Table>
+        </div>
+    )
+}
