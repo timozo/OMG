edb6738043f7e0e08d4bc5eb33b36ba3b7e57229
Alexandre Zürcher <alexandre.zurcher@ik.me>
2023-12-08 19:05:34 +0000

    Revert "Merge branch 'landing-page-stephen' into 'main'"
    This reverts merge request !2

--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -3,12 +3,12 @@ import Logo from "../assets/Logo.svg";
 export default function LandingPage() {
   return (
     <body>
-      <header className="app_header">
+      <header className="appHeader">
         <img src={Logo} alt="OMG logo" className="logo" />
 
-        <div className="btn_wrapper">
-          <button className="login_btn">Login</button>
-          <button className="signup_btn">Sign-up</button>
+        <div className="btnwrapper">
+          <button className="loginbtn">Login</button>
+          <button className="signupbtn">Sign-up</button>
         </div>
       </header>
     </body>
