7dfa0d5b3a4c58acfda5e038ae0a8def27070e55
Stephen Swanson <stephenswanson@Stephens-MacBook-Pro.local>
2023-12-09 00:36:43 +0200

    finishes landing page layout. Waiting for data.

--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -2,14 +2,28 @@ import Header from "../comonents/Header";
 
 export default function LandingPage() {
   return (
-    <body className="landing_page">
+    <>
       <Header />
-      <span className="landing_text">
-        <h1 className="landing_header">Opiskelijoiden Mielipide Gallup</h1>
-        <h3 className="landing_prompt">
-          Find your school or course to get started
-        </h3>
-      </span>
-    </body>
+      <body className="landing_page">
+        <div className="landing_text">
+          <h1 className="landing_header">Opiskelijoiden Mielipide Gallup</h1>
+          <h3 className="landing_prompt">
+            Find your school or course to get started
+          </h3>
+        </div>
+        <div className="landing_search_container">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="landing_search"
+            // value={searchTerm}
+            // onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button /*onClick={handleSearch}*/ className="landing_search_btn">
+            Search
+          </button>
+        </div>
+      </body>
+    </>
   );
 }
