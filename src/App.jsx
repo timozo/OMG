6eb1bb1f8ad8d44d2f0f1eda8b76ffb89f32f03a
Alexandre Zürcher <alexandre.zurcher@ik.me>
2023-12-08 20:52:24 +0200

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import "./App.css";
-import LandingPage from "./components/LandingPage";
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import LandingPage from "./views/LandingPage";
+
 
 function App() {
   return (
-    <>
-      <LandingPage />
-    </>
-  );
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<LandingPage/>} />
+        </Routes>
+      </div>
+    </Router>
+  )
 }
+
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import Home from "./routes/Home"
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import "./index.css";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home/>,
-  },
-]);
-
+import App from "./App"
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
