df81d64f78c82f4e81cb2bd82549225d81337b31
Alexandre Zürcher <alexandre.zurcher@ik.me>
2023-12-09 01:54:16 +0200

    Simplified Search

--- src/views/Search.jsx
+++ src/views/Search.jsx
@@ -1,9 +1,21 @@
-import Table from "../components/Table";
+import { DataGrid } from '@mui/x-data-grid';
 
 export default function Search() {
+    const rows = [
+        { id: 1, col1: 'Hello', col2: 'World', col3: 1000 },
+        { id: 2, col1: 'DataGridPro', col2: 'is Awesome', col3: 1212 },
+        { id: 3, col1: 'MUI', col2: 'is Amazing', col3: 1212},
+      ];
+      
+      const columns = [
+        { field: 'col1', headerName: 'Column 1', width: 150},
+        { field: 'col2', headerName: 'Column 2', width: 150},
+        { field: 'col3', headerName: 'Column 3', width: 150, type: "number"},
+      ];
+
     return (
         <div>
-            <Table></Table>
+            <DataGrid rows={rows} columns={columns} />
         </div>
     )
 }
