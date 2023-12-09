3240c4e6a43cd4d7e177d44bee08cfca4b0d4df2
Stephen Swanson <stephenswanson@Stephens-MacBook-Pro.local>
2023-12-09 04:59:25 +0200

    updates login and signup views

--- src/views/LoginPage.jsx
+++ src/views/LoginPage.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react';
-import { auth, signInWithEmailAndPassword } from '../services/firebase';
+import { useState } from "react";
+import { auth, signInWithEmailAndPassword } from "../services/firebase";
+import "../MagicCard.css";
 
 export default function LoginPage() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   // Handle login with Firebase and error handling
   const handleLogin = () => {
@@ -11,43 +12,52 @@ export default function LoginPage() {
       .then((userCredential) => {
         // Signed in
         const user = userCredential.user;
-        console.log('User:', user);
+        console.log("User:", user);
       })
       .catch((error) => {
-        if (error.code === 'auth/user-not-found' || error.code === 'auth/wrong-password') {
-          alert('E-mail or password is incorrect');
+        if (
+          error.code === "auth/user-not-found" ||
+          error.code === "auth/wrong-password"
+        ) {
+          alert("E-mail or password is incorrect");
         } else {
-          console.error('Error message:', error.message);
+          console.error("Error message:", error.message);
         }
       });
   };
 
   // Navigate to SignupPage in the stack
   const handleSignup = () => {
-    console.log('Signup');
+    console.log("Signup");
   };
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-      <h1 style={{ fontSize: '30px', marginBottom: '30px' }}>Your App Title</h1>
-      <input
-        type="text"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        style={{ height: '50px', width: '250px', marginBottom: '20px' }}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        style={{ height: '50px', width: '250px', marginBottom: '20px' }}
-      />
-      <button onClick={handleLogin}>Login</button>
-      <div>
-        <p>Don't have an account? <span style={{ color: '#007BFF', cursor: 'pointer' }} onClick={handleSignup}>Sign up</span></p>
+    <body>
+      <div className="card">
+        <h1 className="landing_header">Login</h1>
+        <input
+          className="login_form"
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          className="login_form"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="btn login" onClick={handleLogin}>
+          Login
+        </button>
+        <div>
+          <p className="login_prompt">
+            Don't have an account? <span onClick={handleSignup}>Sign up</span>
+          </p>
+        </div>
       </div>
-    </div>
+    </body>
   );
 }
