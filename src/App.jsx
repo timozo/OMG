52ce7b620f24f508f2d6fea0c3af095b7e8d6a61
Stephen Swanson <stephenswanson@Stephens-MacBook-Pro.local>
2023-12-09 03:47:50 +0200

    starts all courses view

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "./views/LoginPage";
 import SignupPage from "./views/SignupPage";
 import Search from "./views/Search";
-import Header from "./components/Header"
+import Header from "./components/Header";
 import RatingsPage from "./views/RatingPage";
+import AllCourses from "./views/AllCourses";
 
 function App() {
   return (
@@ -18,7 +19,7 @@ function App() {
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/rating" element={<RatingsPage />} />
           <Route path="/search" element={<Search />} />
-          <Route path="/courses" />
+          <Route path="/courses" element={<AllCourses />} />
           <Route path="/ezpz" />
         </Routes>
       </Router>
