28bc626a6a259d3d08996b5b98bf09adc2fef1b8
AndreiBrailoiu <darkyss94@gmail.com>
2023-12-09 16:16:23 +0200

    proper_fix_avg_allcourses

--- src/views/AllCourses.jsx
+++ src/views/AllCourses.jsx
@@ -1,251 +1,235 @@
-import { useEffect, useState } from "react";
-import { DataGrid, GridToolbar } from "@mui/x-data-grid";
-import { auth, firestore } from "../services/firebase";
-import { collection, getDocs, addDoc, query, where } from "firebase/firestore";
+// AllCourses.js
+
+import React, { Component } from 'react';
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
+import { auth, firestore } from '../services/firebase';
+import { collection, getDocs, addDoc, query, where } from 'firebase/firestore';
 import Modal from '@mui/material/Modal';
-import arrow from "../assets/arrow_forward.svg";
 import { Rating } from '@mui/material';
 import { Star } from '@mui/icons-material';
-import { TextField } from "@mui/material";
+import { TextField } from '@mui/material';
 import Box from '@mui/material/Box';
 
-
-export default function AllCourses() {
-  const [courses, setCourses] = useState([]);
-
-  const [open, setOpen] = useState(false);
-  const [courseCode, setCourseCode] = useState("")
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
-  function handleButtonClick(courseCode) {
-    setCourseCode(courseCode)
-    setOpen(true)
+class AllCourses extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      courses: [],
+      open: false,
+      courseCode: '',
+      contentRating: 0,
+      qualityRating: 0,
+      workloadRating: 0,
+      comment: '',
+    };
   }
 
-  useEffect(() => {
-    const fetchCourses = async () => {
-      try {
-        const coursesCollection = collection(firestore, "courses");
-        const coursesSnapshot = await getDocs(coursesCollection);
+  componentDidMount() {
+    this.fetchCourses();
+  }
 
-        const courseData = [];
-        const promises = coursesSnapshot.docs.map(async (doc) => {
-          const data = doc.data();
-          const courseCode = data["course-code"];
+  fetchCourses = async () => {
+    try {
+      const coursesCollection = collection(firestore, 'courses');
+      const coursesSnapshot = await getDocs(coursesCollection);
 
-          // Query the ratings collection for the specified courseCode
-          const ratingsCollection = collection(firestore, "ratings");
-          const ratingsQuery = query(
-            ratingsCollection,
-            where("courseCode", "==", courseCode)
-          );
-          const ratingsSnapshot = await getDocs(ratingsQuery);
+      const courseData = [];
+      const promises = coursesSnapshot.docs.map(async (doc) => {
+        const data = doc.data();
+        const courseCode = data['course-code'];
 
-          // Calculate the average rating
-          let totalAvgRating = 0;
+        // Query the ratings collection for the specified courseCode
+        const ratingsCollection = collection(firestore, 'ratings');
+        const ratingsQuery = query(
+          ratingsCollection,
+          where('courseCode', '==', courseCode)
+        );
+        const ratingsSnapshot = await getDocs(ratingsQuery);
 
-          if (ratingsSnapshot.size > 0) {
-            ratingsSnapshot.forEach((ratingDoc) => {
-              const ratingData = ratingDoc.data();
-              totalAvgRating += ratingData.avgRating || 0;
-            });
+        // Calculate the average rating
+        let totalAvgRating = 0;
 
-            totalAvgRating /= ratingsSnapshot.size;
-          }
-
-          courseData.push({
-            id: doc.id,
-            code: courseCode,
-            name: data["name"],
-            credits: data["credits"],
-            rating: totalAvgRating.toFixed(2), // Rounded to 2 decimal places
+        if (ratingsSnapshot.size > 0) {
+          ratingsSnapshot.forEach((ratingDoc) => {
+            const ratingData = ratingDoc.data();
+            totalAvgRating += ratingData.avgRating || 0;
           });
+
+          totalAvgRating /= ratingsSnapshot.size;
+        }
+
+        courseData.push({
+          id: doc.id,
+          code: courseCode,
+          name: data['name'],
+          credits: data['credits'],
+          rating: totalAvgRating.toFixed(2), // Rounded to 2 decimal places
         });
+      });
 
-        await Promise.all(promises);
-        setCourses(courseData);
-      } catch (error) {
-        console.error("Error fetching course details:", error.message);
-      }
-    };
-
-    fetchCourses();
-  }, []);
-
-  const rows = courses.map((course, index) => ({
-    id: index + 1,
-    col1: course.code,
-    col2: course.name,
-    col3: course.credits,
-    col4: course.rating,
-    col5: (
-      <button
-        className="btn filled primary"
-        onClick={() => handleButtonClick(course.code)}
-      >
-        Add Rating
-      </button>
-    ),
-    col6: (
-      <button
-        className="see_more"
-        /*onClick={() => handleButtonClick(course.id)}*/
-      >
-        See more <img src={arrow} alt="arrow forward" />
-      </button>
-    ),
-  }));
-
-  const columns = [
-    { field: "col1", headerName: "Code", width: 150 },
-    { field: "col2", headerName: "Name", width: 150 },
-    { field: "col3", headerName: "Credits", width: 150 },
-    { field: "col4", headerName: "Rating", width: 150 },
-    {
-      field: "col5",
-      headerName: "",
-      width: 200,
-      renderCell: (params) => (
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          {params.value}
-        </div>
-      ),
-    },
-    {
-      field: "col6",
-      headerName: "",
-      width: 200,
-      renderCell: (params) => (
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          {params.value}
-        </div>
-      ),
-    },
-  ];
-
-
-  // MODAL
-  const [contentRating, setContentRating] = useState(0);
-  const [qualityRating, setQualityRating] = useState(0);
-  const [workloadRating, setWorkloadRating] = useState(0);
-  const [comment, setComment] = useState('');
-
-  useEffect(() => {
-      const fetchCourses = async () => {
-          const coursesCollection = collection(firestore, 'courses');
-          const coursesQuery = query(coursesCollection);
-          const coursesSnapshot = await getDocs(coursesQuery);
-
-          const courseData = [];
-          coursesSnapshot.forEach((doc) => {
-              const data = doc.data();
-              courseData.push({
-                  id: doc.id,
-                  code: data['course-code'],
-                  name: data['name'],
-                  // Add other fields if needed
-              });
-          });
-
-          // setCourses(courseData);
-      };
-
-      fetchCourses();
-  }, []);
-
-  const calculateAvgRating = () => {
-      return (contentRating + qualityRating + workloadRating) / 3;
+      await Promise.all(promises);
+      this.setState({ courses: courseData });
+    } catch (error) {
+      console.error('Error fetching course details:', error.message);
+    }
   };
 
-  const handleSubmission = async () => {
-      try {
-          const user = auth.currentUser;
-
-          if (!user) {
-              // Handle user not logged in
-              return;
-          }
-
-          const userUid = user.uid;
-
-          // Add a new rating to the "ratings" collection in Firestore
-          const ratingsCollection = collection(firestore, 'ratings');
-
-          await addDoc(ratingsCollection, {
-              courseCode: courseCode,
-              contentRating,
-              qualityRating,
-              workloadRating,
-              avgRating: calculateAvgRating(), // Add the avgRating field
-              comment,
-              userUid,
-              timestamp: new Date(),
-          });
-
-          // Reset form after submission
-          setContentRating(0);
-          setQualityRating(0);
-          setWorkloadRating(0);
-          setComment('');
-
-          setOpen(false)
-
-          // You can add further logic, like showing a success message or redirecting the user.
-      } catch (error) {
-          console.error('Error submitting rating:', error.message);
-          // Handle error, e.g., show an error message to the user.
-      }
+  handleButtonClick = (courseCode) => {
+    this.setState({ courseCode, open: true });
   };
 
-  return (
-    <>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style} className='rating-modal' style={style}>
-          <h1 style={{ fontSize: '30px', marginBottom: '30px' }}>Rating the course {courseCode}</h1>
+  handleSubmission = async () => {
+    try {
+      const { courseCode, contentRating, qualityRating, workloadRating, comment } =
+        this.state;
 
-          <div className='container'>
-            <div>
+      const user = auth.currentUser;
+
+      if (!user) {
+        // Handle user not logged in
+        return;
+      }
+
+      const userUid = user.uid;
+
+      // Add a new rating to the "ratings" collection in Firestore
+      const ratingsCollection = collection(firestore, 'ratings');
+
+      await addDoc(ratingsCollection, {
+        courseCode,
+        contentRating,
+        qualityRating,
+        workloadRating,
+        avgRating: this.calculateAvgRating(), // Add the avgRating field
+        comment,
+        userUid,
+        timestamp: new Date(),
+      });
+
+      // Reset form after submission
+      this.setState({
+        contentRating: 0,
+        qualityRating: 0,
+        workloadRating: 0,
+        comment: '',
+        open: false,
+      });
+
+      // Fetch courses again to update the course list
+      await this.fetchCourses();
+
+      // You can add further logic, like showing a success message or redirecting the user.
+    } catch (error) {
+      console.error('Error submitting rating:', error.message);
+      // Handle error, e.g., show an error message to the user.
+    }
+  };
+
+  calculateAvgRating = () => {
+    const { contentRating, qualityRating, workloadRating } = this.state;
+    return (contentRating + qualityRating + workloadRating) / 3;
+  };
+
+  render() {
+    const { courses, open, courseCode, contentRating, qualityRating, workloadRating, comment } =
+      this.state;
+
+    const rows = courses.map((course, index) => ({
+      id: index + 1,
+      col1: course.code,
+      col2: course.name,
+      col3: course.credits,
+      col4: course.rating,
+      col5: (
+        <button
+          className="btn filled primary"
+          onClick={() => this.handleButtonClick(course.code)}
+        >
+          Add Rating
+        </button>
+      ),
+      col6: (
+        <button className="see_more">{/* Add your See more button content here */}</button>
+      ),
+    }));
+
+    const columns = [
+      { field: 'col1', headerName: 'Code', width: 150 },
+      { field: 'col2', headerName: 'Name', width: 150 },
+      { field: 'col3', headerName: 'Credits', width: 150 },
+      { field: 'col4', headerName: 'Rating', width: 150 },
+      {
+        field: 'col5',
+        headerName: '',
+        width: 200,
+        renderCell: (params) => (
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            {params.value}
+          </div>
+        ),
+      },
+      {
+        field: 'col6',
+        headerName: '',
+        width: 200,
+        renderCell: (params) => (
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            {params.value}
+          </div>
+        ),
+      },
+    ];
+
+    return (
+      <>
+        <Modal
+          open={open}
+          onClose={() => this.setState({ open: false })}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style} className="rating-modal">
+            <h1 style={{ fontSize: '30px', marginBottom: '30px' }}>
+              Rating the course {courseCode}
+            </h1>
+
+            <div className="container">
+              <div>
                 <p>Content</p>
                 <Rating
                   value={contentRating}
                   precision={1}
-                  onChange={(event, newValue) => { setContentRating(newValue); }}
+                  onChange={(event, newValue) => this.setState({ contentRating: newValue })}
                   emptyIcon={<Star style={{ opacity: 0.55 }} fontSize="inherit" />}
                 />
-            </div>
-            <div>
+              </div>
+              <div>
                 <p>Quality</p>
                 <Rating
                   value={qualityRating}
                   precision={1}
-                  onChange={(event, newValue) => { setQualityRating(newValue); }}
+                  onChange={(event, newValue) => this.setState({ qualityRating: newValue })}
                   emptyIcon={<Star style={{ opacity: 0.55 }} fontSize="inherit" />}
                 />
-            </div>
-            <div>
+              </div>
+              <div>
                 <p>Workload</p>
                 <Rating
                   value={workloadRating}
                   precision={1}
-                  onChange={(event, newValue) => { setWorkloadRating(newValue) }}
+                  onChange={(event, newValue) => this.setState({ workloadRating: newValue })}
                   emptyIcon={<Star style={{ opacity: 0.55 }} fontSize="inherit" />}
                 />
               </div>
@@ -257,25 +241,29 @@ export default function AllCourses() {
               multiline
               maxRows={4}
               variant="filled"
-              onChange={(event) => {setComment(event.target.value)}}
+              value={comment}
+              onChange={(event) => this.setState({ comment: event.target.value })}
             />
-                
-            <a className='btn outline' onClick={() => handleSubmission()}>Submit Rating</a>
-        </Box>
-      </Modal>
-      <div style={{ padding: "2rem" }}>
-        <DataGrid
-          style={{background: "white", padding: "1rem", borderRadius: "27px"}}
-          rows={rows}
-          columns={columns}
-          components={{
-            Toolbar: GridToolbar,
-          }}
-          initialState={{ pagination: { paginationModel: { pageSize: 20 } } }}
-        />
-      </div>
-    </>
-  );
+
+            <a className="btn outline" onClick={this.handleSubmission}>
+              Submit Rating
+            </a>
+          </Box>
+        </Modal>
+        <div style={{ padding: '2rem' }}>
+          <DataGrid
+            style={{ background: 'white', padding: '1rem', borderRadius: '27px' }}
+            rows={rows}
+            columns={columns}
+            components={{
+              Toolbar: GridToolbar,
+            }}
+            initialState={{ pagination: { paginationModel: { pageSize: 20 } } }}
+          />
+        </div>
+      </>
+    );
+  }
 }
 
 const style = {
@@ -289,3 +277,5 @@ const style = {
   boxShadow: 24,
   p: 4,
 };
+
+export default AllCourses;
