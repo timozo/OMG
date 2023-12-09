3a6fc6bd226fa58e3f57a14885b7463d8e215672
Alexandre Zürcher <alexandre.zurcher@ik.me>
2023-12-09 03:30:23 +0200

    improved CSS landing page part 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,32 @@
 import Logo from "../assets/Logo.svg";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <div className="app_header">
-      <img src={Logo} alt="OMG logo" className="logo" />
+      <div className="background_container">
+        <div className="container">
+          <div className="links">
+            <Link to="/"><img src={Logo} alt="OMG logo" className="logo" /></Link>
+            <div className="nav_wrapper">
+              <NavLink to="/courses" className="nav_link">
+                All Courses
+              </NavLink>
+              <NavLink to="/ezpz" className="nav_link">
+                Easy Credits
+              </NavLink>
+            </div>
+          </div>
 
-      <div className="nav_wrapper">
-        <NavLink to="/courses" className="nav_link">
-          All Courses
-        </NavLink>
-        <NavLink to="/ezpz" className="nav_link">
-          Easy Credits
-        </NavLink>
-      </div>
-
-      <div className="btn_wrapper">
-        <NavLink to="/login" className="login_btn">
-          Login
-        </NavLink>
-        <NavLink to="/signup" className="signup_btn">
-          Sign-up
-        </NavLink>
+          <div className="btn_wrapper">
+            <NavLink to="/login" className="btn outline">
+              Login
+            </NavLink>
+            <NavLink to="/signup" className="btn filled">
+              Sign-up
+            </NavLink>
+          </div>
+        </div>
       </div>
     </div>
   );
