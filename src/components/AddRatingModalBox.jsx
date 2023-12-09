d69594e2fcafc2f8cc297d59cddb1017afaa8235
Alexandre Zürcher <alexandre.zurcher@ik.me>
2023-12-09 10:14:42 +0200

    latest shitty version

---
+++ src/components/AddRatingModalBox.jsx
@@ -0,0 +1,148 @@
+import "../Components.css"
+import { useState, useEffect } from 'react';
+import { auth, firestore } from '../services/firebase';
+import { collection, addDoc, getDocs, query } from 'firebase/firestore';
+import { useParams } from 'react-router-dom';
+import { Rating } from '@mui/material';
+import { Star } from '@mui/icons-material';
+import { TextField } from "@mui/material";
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+
+
+
+export default function AddRatingModalBox() {
+    // sets the course code from the route (:id)
+    let { courseCode } = useParams();
+
+    const [contentRating, setContentRating] = useState(0);
+    const [qualityRating, setQualityRating] = useState(0);
+    const [workloadRating, setWorkloadRating] = useState(0);
+    const [comment, setComment] = useState('');
+    // const [courses, setCourses] = useState    
+
+    useEffect(() => {
+        const fetchCourses = async () => {
+            const coursesCollection = collection(firestore, 'courses');
+            const coursesQuery = query(coursesCollection);
+            const coursesSnapshot = await getDocs(coursesQuery);
+
+            const courseData = [];
+            coursesSnapshot.forEach((doc) => {
+                const data = doc.data();
+                courseData.push({
+                    id: doc.id,
+                    code: data['course-code'],
+                    name: data['name'],
+                    // Add other fields if needed
+                });
+            });
+
+            // setCourses(courseData);
+        };
+
+        fetchCourses();
+    }, []);
+
+    const calculateAvgRating = () => {
+        return (contentRating + qualityRating + workloadRating) / 3;
+    };
+
+    const handleSubmission = async () => {
+        try {
+            const user = auth.currentUser;
+
+            if (!user) {
+                // Handle user not logged in
+                return;
+            }
+
+            const userUid = user.uid;
+
+            // Add a new rating to the "ratings" collection in Firestore
+            const ratingsCollection = collection(firestore, 'ratings');
+
+            await addDoc(ratingsCollection, {
+                courseCode,
+                contentRating,
+                qualityRating,
+                workloadRating,
+                avgRating: calculateAvgRating(), // Add the avgRating field
+                comment,
+                userUid,
+                timestamp: new Date(),
+            });
+
+            // Reset form after submission
+            setContentRating(1);
+            setQualityRating(1);
+            setWorkloadRating(1);
+            setComment('');
+
+            // You can add further logic, like showing a success message or redirecting the user.
+        } catch (error) {
+            console.error('Error submitting rating:', error.message);
+            // Handle error, e.g., show an error message to the user.
+        }
+    };
+
+    return (
+        <Box sx={style}>
+            <div className='rating-modal' style={style}>
+                <h1 style={{ fontSize: '30px', marginBottom: '30px' }}>Ratings Page</h1>
+
+                <div className='container'>
+                    <div>
+                        <p>Content</p>
+                        <Rating
+                            value={contentRating}
+                            precision={1}
+                            onChange={(event, newValue) => { setContentRating(newValue); }}
+                            emptyIcon={<Star style={{ opacity: 0.55 }} fontSize="inherit" />}
+                        />
+                    </div>
+                    <div>
+                        <p>Quality</p>
+                        <Rating
+                            value={qualityRating}
+                            precision={1}
+                            onChange={(event, newValue) => { setQualityRating(newValue); }}
+                            emptyIcon={<Star style={{ opacity: 0.55 }} fontSize="inherit" />}
+                        />
+                    </div>
+                    <div>
+                        <p>Workload</p>
+                        <Rating
+                            value={workloadRating}
+                            precision={1}
+                            onChange={(event, newValue) => { setWorkloadRating(newValue); }}
+                            emptyIcon={<Star style={{ opacity: 0.55 }} fontSize="inherit" />}
+                        />
+                    </div>
+                </div>
+
+                <TextField
+                    id="filled-multiline-flexible"
+                    label="Multiline"
+                    multiline
+                    maxRows={4}
+                    variant="filled"
+                />
+                    
+                <button className='btn outline' onClick={handleSubmission}>Submit Rating</button>
+            </div>
+        </Box>
+    );
+}
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
--- src/views/AllCourses.jsx
+++ src/views/AllCourses.jsx
@@ -1,30 +1,64 @@
 import { useEffect, useState } from "react";
-import { DataGrid } from "@mui/x-data-grid";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { firestore } from "../services/firebase";
-import { collection, getDocs, query } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
+// import arrow from "../assets/arrow_forward.svg";
+import AddRatingModalBox from "../components/AddRatingModalBox";
+import Modal from '@mui/material/Modal';
 
 export default function AllCourses() {
   const [courses, setCourses] = useState([]);
 
+  // manages the modal
+  const [open, setOpen] = useState(true);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   useEffect(() => {
     const fetchCourses = async () => {
-      const coursesCollection = collection(firestore, "courses");
-      const coursesQuery = query(coursesCollection);
-      const coursesSnapshot = await getDocs(coursesQuery);
+      try {
+        const coursesCollection = collection(firestore, "courses");
+        const coursesSnapshot = await getDocs(coursesCollection);
 
-      const courseData = [];
-      coursesSnapshot.forEach((doc) => {
-        const data = doc.data();
-        courseData.push({
-          id: doc.id,
-          code: data["course-code"],
-          name: data["name"],
-          credits: data["credits"],
-          rating: data["ratings"],
+        const courseData = [];
+        const promises = coursesSnapshot.docs.map(async (doc) => {
+          const data = doc.data();
+          const courseCode = data["course-code"];
+
+          // Query the ratings collection for the specified courseCode
+          const ratingsCollection = collection(firestore, "ratings");
+          const ratingsQuery = query(
+            ratingsCollection,
+            where("courseCode", "==", courseCode)
+          );
+          const ratingsSnapshot = await getDocs(ratingsQuery);
+
+          // Calculate the average rating
+          let totalAvgRating = 0;
+
+          if (ratingsSnapshot.size > 0) {
+            ratingsSnapshot.forEach((ratingDoc) => {
+              const ratingData = ratingDoc.data();
+              totalAvgRating += ratingData.avgRating || 0;
+            });
+
+            totalAvgRating /= ratingsSnapshot.size;
+          }
+
+          courseData.push({
+            id: doc.id,
+            code: courseCode,
+            name: data["name"],
+            credits: data["credits"],
+            rating: totalAvgRating.toFixed(2), // Rounded to 2 decimal places
+          });
         });
-      });
 
-      setCourses(courseData);
+        await Promise.all(promises);
+        setCourses(courseData);
+      } catch (error) {
+        console.error("Error fetching course details:", error.message);
+      }
     };
 
     fetchCourses();
@@ -38,6 +72,22 @@ export default function AllCourses() {
     col2: course.name,
     col3: course.credits,
     col4: course.rating,
+    col5: (
+      <button
+        className="btn filled primary"
+        onClick={handleOpen()}
+      >
+        Add Rating
+      </button>
+    ),
+    col6: (
+      <button
+
+      /*onClick={() => handleButtonClick(course.id)}*/
+      >
+        See more{/* <img src={arrow} alt="arrow forward" /> */}
+      </button>
+    ),
   }));
 
   const columns = [
@@ -45,13 +95,56 @@ export default function AllCourses() {
     { field: "col2", headerName: "Name", width: 150 },
     { field: "col3", headerName: "Credits", width: 150 },
     { field: "col4", headerName: "Rating", width: 150 },
+    {
+      field: "col5",
+      headerName: "",
+      width: 200,
+      renderCell: (params) => (
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          {params.value}
+        </div>
+      ),
+    },
+    {
+      field: "col6",
+      headerName: "",
+      width: 200,
+      renderCell: (params) => (
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          {params.value}
+        </div>
+      ),
+    },
   ];
 
   return (
     <div style={{ background: "white" }}>
+      <Modal
+        open={handleOpen}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <AddRatingModalBox />
+      </Modal>
       <DataGrid
         rows={rows}
         columns={columns}
+        components={{
+          Toolbar: GridToolbar,
+        }}
         initialState={{ pagination: { paginationModel: { pageSize: 20 } } }}
       />
     </div>
